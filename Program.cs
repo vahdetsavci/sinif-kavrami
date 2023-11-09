11d0a3d5f7a0f580303b676709e8116e879f43a1
Vahdet Savci <savcivahdet@gmail.com>
2023-11-09 14:23:17 +0300

    Console App created

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace sinif_kavrami;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*  Söz Dizimi
+            class SinifAdi
+            {
+                [Erişim Belirleyici] [Veri Tipi] ÖzellikAdi;
+                [Erişim Belirleyici] [Veri Tipi] MetotAdi(Parametre Listesi)
+                {
+                    Metot Komutları
+                }
+            }
+        */
+
+        /*
+            Erişim Belirleyiciler
+            * Public
+            * Private
+            * Internal
+            * Protected
+        */
+
+        Calisan calisan1 = new Calisan();
+        calisan1.Ad = "Ayşe";
+        calisan1.Soyad = "Kara";
+        calisan1.No = 234225634;
+        calisan1.Departman = "İnsan Kaynakları";
+
+        calisan1.CalisanBilgileri();
+        Console.WriteLine("************************");
+
+        Calisan calisan2 = new Calisan();
+        calisan2.Ad = "Derya";
+        calisan2.Soyad = "Yılmaz";
+        calisan2.No = 256646984;
+        calisan2.Departman = "Satın Alma";
+
+        calisan2.CalisanBilgileri();
+    }
+}
+
+class Calisan
+{
+    internal string Ad;
+    internal string Soyad;
+    internal int No;
+    internal string Departman;
+
+    internal void CalisanBilgileri()
+    {
+        Console.WriteLine("Çalışanın Adı: {0}", Ad);
+        Console.WriteLine("Çalışanın Soyadı: {0}", Soyad);
+        Console.WriteLine("Çalışanın Numarası: {0}", No);
+        Console.WriteLine("Çalışanın Departmanı: {0}", Departman);
+    }
+}
